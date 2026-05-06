c20753ffb2be7a38efe7338d2ff7f8aa3e9bf81b
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-05-06 20:10:26 +0200

    Server express

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenidos a la API' });
+});
+
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT ||3001;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
